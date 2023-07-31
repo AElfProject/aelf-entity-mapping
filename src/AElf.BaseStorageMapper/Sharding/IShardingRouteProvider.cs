e32bdc20e4612ebbbe88cbcb582ab003c1b6c268
AElfBourneShi <Bourne.shi@aelf.io>
2023-07-31 10:14:48 +0800

    fix: sharding route provider parameters

--- src/AElf.BaseStorageMapper/Sharding/IShardingRouteProvider.cs
+++ src/AElf.BaseStorageMapper/Sharding/IShardingRouteProvider.cs
@@ -8,9 +8,9 @@ public interface IShardingRouteProvider
     /// <param name="conditions"></param>
     /// <typeparam name="TEntity">ElasticSearch Index Entity</typeparam>
     /// <returns></returns>
-    Task<List<string>> GetShardCollectionListByEqualConditionAsync<TEntity>(List<Dictionary<string,object>> conditions);
+    Task<List<string>> GetShardCollectionListByEqualConditionAsync<TEntity>(Dictionary<string,object> conditions);
     
-    Task<List<string>> GetShardCollectionListByGreaterThanConditionAsync<TEntity>(List<Dictionary<string,object>> conditions);
+    Task<List<string>> GetShardCollectionListByGreaterThanConditionAsync<TEntity>(Dictionary<string,object> conditions);
     
-    Task<List<string>> GetShardCollectionListByLessThanConditionAsync<TEntity>(List<Dictionary<string,object>> conditions);
+    Task<List<string>> GetShardCollectionListByLessThanConditionAsync<TEntity>(Dictionary<string,object> conditions);
 }
