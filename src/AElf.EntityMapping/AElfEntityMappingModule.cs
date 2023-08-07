92c8aafd0a4f0d9788f455a28600ab4382fd8baf
yann <yann@git.com>
2023-08-07 20:05:59 +0800

    add shardConfig check

--- src/AElf.EntityMapping/AElfEntityMappingModule.cs
+++ src/AElf.EntityMapping/AElfEntityMappingModule.cs
@@ -1,5 +1,7 @@
 ﻿using AElf.EntityMapping.Options;
+using AElf.EntityMapping.Sharding;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Volo.Abp;
 using Volo.Abp.Caching;
 using Volo.Abp.Modularity;
@@ -19,15 +21,87 @@ namespace AElf.EntityMapping
             
             var services = context.Services;
             services.AddTransient(typeof(ICollectionNameProvider<>), typeof(DefaultCollectionNameProvider<>));
-            
-            // Configure<IndexSettingOptions>(configuration.GetSection("IndexSetting"));
-            //Configure<ShardInitSettingOptions>(configuration.GetSection("ShardSetting"));
+            configuration.GetSection("");
         }
         
         public override void OnPreApplicationInitialization(ApplicationInitializationContext context)
         {
-            
+           // CheckAElfEntityMappingOptions(context);
         }
-        
+
+        public void CheckAElfEntityMappingOptions(ApplicationInitializationContext context)
+        {
+            try
+            {
+                var option = context.ServiceProvider.GetRequiredService<IOptionsSnapshot<AElfEntityMappingOptions>>();
+                if (option.Value == null)
+                    throw new Exception("AElfEntityMappingOptions config cant be null");
+                
+                var shardInitSettingDtos =  option.Value.ShardInitSettings;
+                if (shardInitSettingDtos.IsNullOrEmpty())
+                    return ;
+
+                foreach (var shardInitSettingDto in shardInitSettingDtos)
+                {
+                    var shardGroups = shardInitSettingDto.ShardGroups;
+                    if (shardGroups == null || shardGroups.Count == 0)
+                    {
+                        throw new Exception("AElfEntityMappingOptions.ShardGroups config cant be null");
+                    }
+                    
+                    Dictionary<string,string> shardKeyDic = new Dictionary<string, string>();
+                    foreach (var shardGroup in shardGroups)
+                    {
+                        var shardKeys = shardGroup.ShardKeys;
+                        if (shardKeys == null || shardKeys.Count == 0)
+                        {
+                            throw new Exception("AElfEntityMappingOptions.ShardGroups.ShardKeys config cant be null");
+                        }
+                        var checkStepValue = !shardKeys.Last().Value.IsNullOrEmpty() && !shardKeys.Last().Value.IsNullOrWhiteSpace() && 
+                                             (int.TryParse(shardKeys.Last().Value, out var stepValue) && stepValue > 0);
+                        if (!checkStepValue)
+                        {
+                            throw new Exception("AElfEntityMappingOptions.ShardGroups.ShardKeys.Step config is not correct, if step>0 need config at the end");
+                        }
+
+                        var shardKeyGroupNoCount =
+                            shardGroup.ShardKeys.FindAll(a => a.GroupNo == shardGroup.ShardKeys.First().GroupNo).Count;
+                        if (shardKeyGroupNoCount != shardGroup.ShardKeys.Count)
+                        {
+                            throw new Exception("AElfEntityMappingOptions.ShardGroups.ShardKeys.GroupNo config is not correct,  GroupNo must be consistent");
+                        }
+
+                        var similarShardKeys = shardKeys.FindAll(a=>!a.Value.IsNullOrEmpty() && !a.Value.IsNullOrWhiteSpace() && (int.TryParse(a.Value, out var stepValue) && stepValue > 0));
+                        if(similarShardKeys.Count == 0)
+                            throw new Exception("AElfEntityMappingOptions.ShardGroups.ShardKeys.Step config is not correct,  step must greater than 0");
+                        
+                        if(similarShardKeys.Count > 1)
+                            throw new Exception("AElfEntityMappingOptions.ShardGroups.ShardKeys.Step config is not correct, greater than 0 step config can only one");
+
+                        foreach (var shardKey in shardKeys)
+                        {
+                            if (shardKey.Value.IsNullOrEmpty() || shardKey.Value == "0") continue;
+                            var value = shardKeyDic[shardKey.Name];
+                            if (value.IsNullOrEmpty())
+                            {
+                                shardKeyDic.Add(shardKey.Name, shardKey.Value);
+                            }
+                            else
+                            {
+                                if (value == shardKey.Value)
+                                {
+                                    throw new Exception($"AElfEntityMappingOptions.ShardGroups.ShardKeys.Value config is not correct,  Value:{value} must be not consistent");
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                
+            }catch(Exception e)
+            {
+                throw new Exception("CheckAElfEntityMappingOptions Exception", e);
+            }
+        }
     }
 }
