2dbd2e6fd122c53c10ef69aff076d90a8c7ee15e
zhxymh <sean.zhang@aelf.io>
2024-05-08 11:54:20 +0800

    feat: add settings for create index

--- src/AElf.EntityMapping.Elasticsearch/Services/IElasticIndexService.cs
+++ src/AElf.EntityMapping.Elasticsearch/Services/IElasticIndexService.cs
@@ -2,7 +2,8 @@ namespace AElf.EntityMapping.Elasticsearch.Services;
 
 public interface IElasticIndexService
 {
-    Task CreateIndexAsync(string indexName, Type indexEntityType, int shard = 1, int numberOfReplicas = 1);
+    Task CreateIndexAsync(string indexName, Type indexEntityType, int shard = 1, int numberOfReplicas = 1,
+        Dictionary<string, object> indexSettings = null);
 
     Task CreateIndexTemplateAsync(string indexTemplateName, string indexName, Type indexEntityType, int numberOfShards,
         int numberOfReplicas);
